5dd68de79e001e5caaa67e946140947f84bde408
Quanyin Tang <qytang326@gmail.com>
2017-10-10 15:37:36 +0800

    整合页面css js与优化

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'github-pages' , group: :jekyll_plugins
 gem 'wdm', '~> 0.1.0' if Gem.win_platform?
+gem 'tzinfo-data'
 gem 'jekyll-paginate'
 gem 'jekyll-github-metadata'
 gem 'jekyll-mentions'
--- Gruntfile.js
+++ Gruntfile.js
@@ -4,36 +4,66 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         uglify: {
-            main: {
-                src: 'js/<%= pkg.name %>.js',
-                dest: 'js/<%= pkg.name %>.min.js'
-            },
-			build: {
-                src: 'js/<%= pkg.name %>.js',
-                dest: 'js/<%= pkg.name %>.min.js'
-            }
-        },
+            main1: {
+                src: 'js/jquery.nav.js',
+                dest: 'js/jquery.nav.min.js'
+                    },
+            main2: {
+                src: 'js/jquery.tagcloud.js',
+                dest: 'js/jquery.tagcloud.min.js'
+                    },
+            /* 会报错,注释掉,只能单独处理
+            main3: {
+                src: 'js/snackbar-sw-registration.js',
+                dest: 'js/snackbar-sw-registration.min.js'
+                    },  
+            */                
+            main4: {
+                src: 'js/Quanyin-global.js',
+                dest: 'js/Quanyin-global.min.js'
+                    }
+                },                
+        concat: {
+ /*           options: {
+            separator: ';',
+                      },
+*/                    
+            dist1: {
+                src: ['js/snackbar.js', 'js/sw-registration.js'],
+                dest: 'js/snackbar-sw-registration.js',
+                    },
+            dist2: {
+                src: ['js/loadCSS.js', 'js/csspreload.js'],
+                dest: 'js/plugin.js',
+                    },                    
+            dist3: {
+                src: ['js/Quanyin-Blog.js','js/plugin.js'],
+                dest: 'js/Quanyin-global.js',
+                    },        
+                },
+        jshint: {
+          all: ['js/Quanyin-global.js']
+        },                
         less: {
             expanded: {
                 options: {
                     paths: ["css"]
                 },
                 files: {
-                    "css/<%= pkg.name %>.css": "less/<%= pkg.name %>.less"
-                }
-            },
+                    "css/Quanyin-global.css": "less/Quanyin-global.less"
+                        }
+                      },
             minified: {
                 options: {
                     paths: ["css"],
                     cleancss: true
                 },
                 files: {
-                    "css/<%= pkg.name %>.min.css": "less/<%= pkg.name %>.less"
-                }
-            }
-        },
-        banner: '/*!\n' +
-            ' * <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
+                    "css/Quanyin-global.min.css": "less/Quanyin-global.less"
+                        }
+                    }
+                },
+        banner: '/*! <%= pkg.title %> v<%= pkg.version %> (<%= pkg.homepage %>)\n' +
             ' * Copyright <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
             ' */\n',
         usebanner: {
@@ -43,13 +73,13 @@ module.exports = function(grunt) {
                     banner: '<%= banner %>'
                 },
                 files: {
-                    src: ['css/<%= pkg.name %>.css', 'css/<%= pkg.name %>.min.css', 'js/<%= pkg.name %>.min.js']
+                    src: ['css/Quanyin-global.css', 'css/Quanyin-global.min.css', 'js/Quanyin-global.min.js']
                 }
             }
         },
         watch: {
             scripts: {
-                files: ['js/<%= pkg.name %>.js'],
+                files: ['js/Quanyin-global.js'],
                 tasks: ['uglify'],
                 options: {
                     spawn: false,
@@ -66,14 +96,16 @@ module.exports = function(grunt) {
     });
 
     // Load the plugins.
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-banner');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-watch');
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-
+    grunt.loadNpmTasks('grunt-contrib-connect');
     // Default task(s).
-    grunt.registerTask('default', ['uglify', 'less', 'usebanner']);
+    grunt.registerTask('default', ['concat','uglify','jshint','less', 'usebanner']);
 
 };
